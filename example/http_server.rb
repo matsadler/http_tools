970dadde141126b365c7b7ae8ad280c36188c4b0
Matthew Sadler <mat@sourcetagsandcodes.com>
2011-04-10 15:19:10 +0100

    example server: use Parser#env

--- example/http_server.rb
+++ example/http_server.rb
@@ -6,42 +6,9 @@ require 'http_tools'
 
 module HTTP
   class Server
-    
-    REQUEST_METHOD = "REQUEST_METHOD".freeze
-    SCRIPT_NAME = "SCRIPT_NAME".freeze
-    PATH_INFO = "PATH_INFO".freeze
-    QUERY_STRING = "QUERY_STRING".freeze
-    REQUEST_URI = "REQUEST_URI".freeze
-    FRAGMENT = "FRAGMENT".freeze
     SERVER_NAME = "SERVER_NAME".freeze
     SERVER_PORT = "SERVER_PORT".freeze
-    RACK_VERSION = "rack.version".freeze
-    RACK_URL_SCHEME = "rack.url_scheme".freeze
     RACK_INPUT = "rack.input".freeze
-    RACK_ERRORS = "rack.errors".freeze
-    RACK_MULTITHREAD = "rack.multithread".freeze
-    RACK_MULTIPROCESS = "rack.multiprocess".freeze
-    RACK_RUN_ONCE = "rack.run_once".freeze
-    
-    PROTOTYPE_ENV = {
-      REQUEST_METHOD => nil,
-      SCRIPT_NAME => "".freeze,
-      PATH_INFO => "/".freeze,
-      QUERY_STRING => "".freeze,
-      SERVER_NAME => "".freeze,
-      SERVER_PORT => "".freeze,
-      RACK_VERSION => [1, 1].freeze,
-      RACK_URL_SCHEME => "http".freeze,
-      RACK_INPUT => nil,
-      RACK_ERRORS => STDERR,
-      RACK_MULTITHREAD => false,
-      RACK_MULTIPROCESS => false,
-      RACK_RUN_ONCE => false}.freeze
-    
-    GET = "GET".freeze
-    HTTP_ = "HTTP_".freeze
-    LOWERCASE = "a-z-".freeze
-    UPPERCASE = "A-Z_".freeze
     NO_BODY = {"GET" => true, "HEAD" => true}
     
     attr_reader :app, :host, :port, :server
@@ -51,7 +18,8 @@ module HTTP
       @app = app
       @host = options[:host] || options[:Host] || "0.0.0.0"
       @port = (options[:port] || options[:Port] || 8080).to_s
-      @default_env = options[:default_env] || {}
+      @default_env = {SERVER_NAME => host, SERVER_PORT => port}
+      @default_env.merge!(options[:default_env]) if options[:default_env]
       @multithread = options[:multithread]
       @server = TCPServer.new(host, port)
       @timeout = 10
@@ -72,13 +40,6 @@ module HTTP
       end
     end
     
-    def default_env
-      PROTOTYPE_ENV.merge(
-        SERVER_NAME => host,
-        SERVER_PORT => port,
-        RACK_INPUT => StringIO.new).merge!(@default_env)
-    end
-    
     private
     def on_connection(socket)
       parser = HTTPTools::Parser.new
@@ -86,15 +47,7 @@ module HTTP
       
       parser.on(:header) do
         parser.force_no_body = NO_BODY[parser.request_method]
-        env = default_env
-        env[REQUEST_METHOD] = parser.request_method
-        env[REQUEST_URI] = parser.request_uri
-        if parser.path_info
-          env[PATH_INFO] = parser.path_info
-          env[QUERY_STRING] = parser.query_string
-        end
-        env[FRAGMENT] = parser.fragment if parser.fragment
-        merge_in_rack_format(env, parser.header)
+        env = parser.env.merge!(RACK_INPUT => StringIO.new).merge!(@default_env)
       end
       parser.on(:stream) {|chunk| env[RACK_INPUT] << chunk}
       parser.on(:finish) do |remainder|
@@ -119,10 +72,6 @@ module HTTP
         break
       end until parser.finished?
       nil
-    end
-    
-    def merge_in_rack_format(env, headers)
-      headers.each {|k, val| env[HTTP_ + k.tr(LOWERCASE, UPPERCASE)] = val}; env
     end
     
   end
