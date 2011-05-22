19a18daeedde52c143a935df930f237eb525e543
Matthew Sadler <mat@sourcetagsandcodes.com>
2011-05-22 16:27:31 +0100

    Slightly rejig the Exception hierarchy
    In the future we may want to raise custom errors that aren't to do with parsing

--- lib/http_tools/errors.rb
+++ lib/http_tools/errors.rb
@@ -1,5 +1,6 @@
 module HTTPTools
-  class ParseError < StandardError; end
+  class Error < StandardError; end
+  class ParseError < Error; end
   class EndOfMessageError < ParseError; end
   class MessageIncompleteError < EndOfMessageError; end
   class EmptyMessageError < MessageIncompleteError; end
