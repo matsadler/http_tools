d301e174b6e1fb61e68c98639319b72c2aa262bd
Matthew Sadler <mat@sourcetagsandcodes.com>
2011-06-07 21:53:33 +0100

    improve response body benchmarks

--- bench/parser/large_response_bench.rb
+++ bench/parser/large_response_bench.rb
@@ -2,39 +2,39 @@ base = File.expand_path(File.dirname(__FILE__) + '/../../lib')
 require base + '/http_tools'
 require 'benchmark'
 
-body = "x" * 1024 * 1024 * 1
-response = "HTTP/1.1 200 OK\r\nDate: Mon, 06 Jun 2011 14:55:51 GMT\r\nServer: Apache/2.2.17 (Unix) mod_ssl/2.2.17 OpenSSL/0.9.8l DAV/2 mod_fastcgi/2.4.2\r\nLast-Modified: Mon, 06 Jun 2011 14:55:49 GMT\r\nETag: \"3f18045-400-4a50c4c87c740\"\r\nAccept-Ranges: bytes\r\nContent-Length: #{body.length}\r\nContent-Type: text/plain\r\n\r\n"
-chunks = []
-64.times {|i| chunks << body[i * 64, body.length / 64]}
-
-Benchmark.bm(25) do |x|
-  x.report("HTTPTools::Parser") do
+Benchmark.bm(26) do |x|
+  body = "x" * 1024 * 1024
+  chunks = []
+  64.times {|i| chunks << body[i * 64, body.length / 64]}
+  
+  header = "HTTP/1.1 200 OK\r\nDate: Mon, 06 Jun 2011 14:55:51 GMT\r\nServer: Apache/2.2.17 (Unix) mod_ssl/2.2.17 OpenSSL/0.9.8l DAV/2 mod_fastcgi/2.4.2\r\nLast-Modified: Mon, 06 Jun 2011 14:55:49 GMT\r\nETag: \"3f18045-400-4a50c4c87c740\"\r\nAccept-Ranges: bytes\r\nContent-Length: #{body.length}\r\nContent-Type: text/plain\r\n\r\n"
+  x.report("content_length") do
     200.times do
-       parser = HTTPTools::Parser.new
-       parser << response
-       chunks.each {|chunk| parser << chunk}
+      parser = HTTPTools::Parser.new
+      parser << header
+      chunks.each {|chunk| parser << chunk}
     end
   end
   
-  x.report("HTTPTools::Parser (reset)") do
-    parser = HTTPTools::Parser.new
+  header = "HTTP/1.1 200 OK\r\nDate: Mon, 06 Jun 2011 14:55:51 GMT\r\nServer: Apache/2.2.17 (Unix) mod_ssl/2.2.17 OpenSSL/0.9.8l DAV/2 mod_fastcgi/2.4.2\r\nLast-Modified: Mon, 06 Jun 2011 14:55:49 GMT\r\nETag: \"3f18045-400-4a50c4c87c740\"\r\nAccept-Ranges: bytes\r\nConnection: close\r\nContent-Type: text/plain\r\n\r\n"
+  x.report("close") do
     200.times do
-       parser << response
-       chunks.each {|chunk| parser << chunk}
-       parser.reset
+      parser = HTTPTools::Parser.new
+      parser << header
+      chunks.each {|chunk| parser << chunk}
+      parser.finish
     end
   end
   
-  begin
-    require 'rubygems'
-    require 'http/parser'
-    x.report("Http::Parser") do
-      200.times do
-        parser = Http::Parser.new
-        parser << response
-        chunks.each {|chunk| parser << chunk}
-      end
+  header = "HTTP/1.1 200 OK\r\nDate: Mon, 06 Jun 2011 14:55:51 GMT\r\nServer: Apache/2.2.17 (Unix) mod_ssl/2.2.17 OpenSSL/0.9.8l DAV/2 mod_fastcgi/2.4.2\r\nLast-Modified: Mon, 06 Jun 2011 14:55:49 GMT\r\nETag: \"3f18045-400-4a50c4c87c740\"\r\nAccept-Ranges: bytes\r\nTransfer-Encoding: chunked\r\nContent-Type: text/plain\r\n\r\n"
+  chunks << nil
+  chunks.map!(&HTTPTools::Encoding.method(:transfer_encoding_chunked_encode))
+  x.report("chunked") do
+    200.times do
+      parser = HTTPTools::Parser.new
+      parser << header
+      chunks.each {|chunk| parser << chunk}
     end
-  rescue LoadError
   end
+  
 end
--- bench/transfer_encoding_chunked_bench.rb
+++ bench/transfer_encoding_chunked_bench.rb
@@ -3,22 +3,22 @@ require base + '/http_tools'
 require 'benchmark'
 
 Benchmark.bm(36) do |x|
+  encoded = "1\r\na\r\n" * 100 + "0\r\n"
   x.report("lots of very short chunks") do
-    encoded = "1\r\na\r\n" * 100 + "0\r\n"
     1_000.times do
        HTTPTools::Encoding.transfer_encoding_chunked_decode(encoded)
     end
   end
   
+  encoded = "16\r\n<h1>Hello world</h1>\r\n\r\n12\r\n<p>Lorem ipsum</p>\r\n" * 50 + "0\r\n"
   x.report("slightly less slightly longer chunks") do
-    encoded = "16\r\n<h1>Hello world</h1>\r\n\r\n12\r\n<p>Lorem ipsum</p>\r\n" * 50 + "0\r\n"
     1_000.times do
        HTTPTools::Encoding.transfer_encoding_chunked_decode(encoded)
     end
   end
   
+  encoded = "2710\r\n#{"a" * 10000}\r\n" * 2 + "0\r\n"
   x.report("a couple of big chunks") do
-    encoded = "2710\r\n#{"a" * 10000}\r\n" * 2 + "0\r\n"
     1_000.times do
        HTTPTools::Encoding.transfer_encoding_chunked_decode(encoded)
     end
